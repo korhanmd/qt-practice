d34fc4b14ebe3ac290de1d329586c1ec13e97c5f
Korhan Mutludoğan <korhanmd@gmail.com>
2021-07-05 11:41:02 +0300

    Add QSS to label

--- hello_world/hello.cpp
+++ hello_world/hello.cpp
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
 	QLabel label("Hello world !");
+	label.setStyleSheet("QLabel:hover { color: rgb(60, 179, 113)}");
 	label.show();
 	return app.exec();
 }
