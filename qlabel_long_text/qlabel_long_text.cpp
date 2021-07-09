a79774dd01cddd26956635e3bbe957a27ccbb856
Korhan Mutludoğan <korhanmd@gmail.com>
2021-07-09 12:00:03 +0300

    Fix the string

--- qlabel_long_text/qlabel_long_text.cpp
+++ qlabel_long_text/qlabel_long_text.cpp
@@ -6,9 +6,7 @@ int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
 	
-	QString message = "'What do you know about this business?' the
-	King said to Alice.\n'Nothing,' said Alice.\n'Nothing whatever?' persisted
-	the King.\n'Nothing whatever,' said Alice.";
+	QString message = "'What do you know about this business?' the King said to Alice.\n'Nothing,' said Alice.\n'Nothing whatever?' persisted the King.\n'Nothing whatever,' said Alice.";
 	
 	QLabel label(message);
 	label.setFont(QFont("Comic Sans MS", 18));
