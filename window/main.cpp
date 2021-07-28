d8d91bd1e35249657c7db5ff0085d73b20376d4c
Korhan Mutludoğan <korhanmd@gmail.com>
2021-07-28 12:15:29 +0300

    Start to work with windows

---
+++ window/main.cpp
@@ -0,0 +1,13 @@
+#include <QApplication>
+#include <QMainWindow>
+#include <QLabel>
+
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+	
+	QMainWindow mainWindow;
+	mainWindow.show();
+	
+	return a.exec();
+}
