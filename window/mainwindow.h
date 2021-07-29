db37e37b39bca3beeab4afb8a66ad88581491c6b
Korhan Mutludoğan <korhanmd@gmail.com>
2021-07-29 15:03:29 +0300

    Add private menus and actions to the header of MainWindow class

--- window/mainwindow.h
+++ window/mainwindow.h
@@ -2,13 +2,31 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QLabel>
+#include <QApplication>
+#include <QAction>
+#include <QtGui>
+#include <QMenuBar>
+#include <QMenu>
+#include <Qt>
 
 class MainWindow : public QMainWindow
 {
 	Q_OBJECT
 	public:
 		MainWindow();
+	private slots:
+	private:
+		// Menus
+		QMenu *fileMenu;
+		QMenu *helpMenu;
+		// Actions
+		QAction *quitAction;
+		QAction *aboutAction;
+		QAction *saveAction;
+		QAction *cancelAction;
+		QAction *openAction;
+		QAction *newAction;
+		QAction *aboutQtAction;
 };
 
 #endif
