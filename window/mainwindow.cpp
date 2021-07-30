ad2ab5bc7b039ed8c4cf3c5d119a5c6e39e8d832
Korhan Mutludoğan <korhanmd@gmail.com>
2021-07-30 15:31:39 +0300

    Setup core widgets

--- window/mainwindow.cpp
+++ window/mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow()
 	setFixedSize(500, 500);
 
 	createIcons();
+	setupCoreWidgets();
 
 	// Setup File Menu
 	fileMenu = menuBar()->addMenu("&File");
@@ -50,4 +51,50 @@ void MainWindow::createIcons() {
 	openIcon = QPixmap("open.png");
 	closeIcon = QPixmap("close.png");
 	clearIcon = QPixmap("clear.png");
+}
+
+void MainWindow::setupCoreWidgets() {
+	mainWidget = new QWidget();
+	centralWidgetLayout = new QVBoxLayout();
+	formLayout = new QGridLayout();
+	buttonsLayout = new QHBoxLayout();
+	nameLabel = new QLabel("Name:");
+	dateOfBirthLabel= new QLabel("Date Of Birth:");
+	phoneNumberLabel = new QLabel("Phone Number");
+	savePushButton = new QPushButton("Save");
+	newPushButton = new QPushButton("Clear All");
+	nameLineEdit = new QLineEdit();
+	dateOfBirthEdit = new QDateEdit(QDate::currentDate());
+	phoneNumberLineEdit = new QLineEdit();
+
+	// TableView
+	appTable = new QTableView();
+	model = new QStandardItemModel(1, 3, this);
+
+	appTable->setContextMenuPolicy(Qt::CustomContextMenu);
+	appTable->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+	model->setHorizontalHeaderItem(0, new QStandardItem(QString("Name")));
+	model->setHorizontalHeaderItem(1, new QStandardItem(QString("Date of Birth")));
+	model->setHorizontalHeaderItem(2, new QStandardItem(QString("Phone Number")));
+	appTable->setModel(model)
+
+	QStandardItem *firstItem = new QStandardItem(QString("G. Shone"));
+	QDate dateOfBirth(1980, 1, 1);
+	QStandardItem *secondItem = new QStandardItem(dateOfBirth.toString());
+	QStandardItem *thirdItem = new QStandardItem(QString("05443394858"));
+
+	model->setItem(0,0,firstItem);
+	model->setItem(0,1,secondItem);
+	model->setItem(0,2,thirdItem);
+
+	formLayout->addWidget(nameLabel, 0, 0);
+	formLayout->addWidget(nameLineEdit, 0, 1);
+	formLayout->addWidget(dateOfBirthLabel, 1, 0);
+	formLayout->addWidget(dateOfBirthEdit, 1, 1);
+	formLayout->addWidget(phoneNumberLabel, 2, 0);
+	formLayout->addWidget(phoneNumberLineEdit, 2, 1);
+
+	buttonsLayout->addStretch();
+	buttonsLayout->addWidget(savePushButton);
+	buttonsLayout->addWidget(newPushButton);
 }
