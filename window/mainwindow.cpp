83c5b482ad30138edfa18f44d4684db8ecc1c523
Korhan Mutludoğan <korhanmd@gmail.com>
2021-07-30 11:04:16 +0300

    Add toolbar to the window

--- window/mainwindow.cpp
+++ window/mainwindow.cpp
@@ -34,6 +34,14 @@ MainWindow::MainWindow()
 
 	helpMenu->addAction(aboutAction);
 
+	// Setup Tool bar menu
+	toolbar = addToolBar("main toolbar");
+
+	newToolBarAction = toolbar->addAction(QIcon(newIcon), "New File");
+	openToolBarAction = toolbar->addAction(QIcon(openIcon), "Open File");
+	toolbar->addSeparator();
+	closeToolBarAction = toolbar->addAction(QIcon(closeIcon), "Quit Application");
+
 	// Setup Signals and Slots
 	connect(quitAction, &QAction::triggered, this, &QApplication::quit);
 }
