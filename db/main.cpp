1856770ae8b460618c3a8796f74488acb3077465
Korhan Mutludoğan <korhanmd@gmail.com>
2021-08-11 17:00:19 +0300

    Connect to the database

---
+++ db/main.cpp
@@ -0,0 +1,32 @@
+#include <QApplication>
+#include <QtSql>
+#include <QDebug>
+/*
+use contact_db;
+CREATE TABLE IF NOT EXISTS contacts (
+id INT AUTO_INCREMENT,
+last_name VARCHAR(255) NOT NULL,
+first_name VARCHAR(255) NOT NULL,
+phone_number VARCHAR(255) NOT NULL,
+PRIMARY KEY (id)
+) ENGINE=INNODB;
+*/
+
+int main(int argc, char *argv[]) {
+	// Setup db connection
+	QSqlDatabase db_conn =
+	QSqlDatabase::addDatabase("QMYSQL", "contact_db");
+	db_conn.setHostName("127.0.0.1");
+	db_conn.setDatabaseName("contact_db");
+	db_conn.setUserName("root");
+	db_conn.setPassword("");
+	db_conn.setPort(3306);
+
+	// Error checks
+	if (!db_conn.open()) {
+		qDebug() << db_conn.lastError();
+		return 1;
+	} else {
+		qDebug() << "Database connection established !";
+	}
+}
